5fc70bebaf30c1f7da0164b1079867d024beb0ba
dbondi <dbondi@wisc.edu>
2021-04-04 21:50:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
   name = 'analyst_recommendation_performance',        
   packages = ['analyst_recommendation_performance'],   
-  version = '1.2',      
+  version = '1.3',      
   license='MIT',        
   description = 'Get the performance of analysts stock recommendation',  
   long_description= 'view docs at https://github.com/dbondi/analyst_recommendation_performance',
   author = 'Daniel Bondi',             
   author_email = 'dbondi@wisc.edu', 
   url = 'https://github.com/dbondi/analyst_recommendation_performance',  
-  download_url = 'https://github.com/dbondi/analyst_recommendation_performance/archive/refs/tags/v1.2.tar.gz',    
+  download_url = 'https://github.com/dbondi/analyst_recommendation_performance/archive/refs/tags/v1.3.tar.gz',    
   keywords = ['STOCKS', 'ANALYSTS', 'PYTHON', 'RECOMMENDATIONS','TICKERS'],  
   install_requires=[          
           'dateutil',
