ef74cffeacb2c6c714be1f8326ca540d8f712635
dbondi <dbondi@wisc.edu>
2021-04-04 21:48:36 -0400

    new version

--- setup.py
+++ setup.py
@@ -2,21 +2,18 @@ from distutils.core import setup
 setup(
   name = 'analyst_recommendation_performance',        
   packages = ['analyst_recommendation_performance'],   
-  version = '1.1',      
+  version = '1.2',      
   license='MIT',        
   description = 'Get the performance of analysts stock recommendation',  
   long_description= 'view docs at https://github.com/dbondi/analyst_recommendation_performance',
   author = 'Daniel Bondi',             
   author_email = 'dbondi@wisc.edu', 
   url = 'https://github.com/dbondi/analyst_recommendation_performance',  
-  download_url = 'https://github.com/dbondi/analyst_recommendation_performance/archive/refs/tags/v1.1.tar.gz',    
+  download_url = 'https://github.com/dbondi/analyst_recommendation_performance/archive/refs/tags/v1.2.tar.gz',    
   keywords = ['STOCKS', 'ANALYSTS', 'PYTHON', 'RECOMMENDATIONS','TICKERS'],  
   install_requires=[          
-          'dateutil.relativedelta',
-          'matplotlib.ticker',
-          'matplotlib.pyplot',
-          'matplotlib.patches',
-          'matplotlib.scale',
+          'dateutil',
+          'matplotlib',
           'yfinance'
           'numpy',
           'pandas'
